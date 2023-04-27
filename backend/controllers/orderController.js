c0b9772cfc60e20a6c20b7a53d3b3cdf6a2d57b9
Derek <baldka1109bh@gmail.com>
2023-04-27 12:07:28 +0800

    Update order all data

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -12,6 +12,22 @@ const createOrder = asyncHandler(async (req, res) => {
       digits: req.body.truck_num_digits,
       letters: req.body.truck_num_letters,
     },
+    load_name: req.body.load_name,
+    load_weight: req.body.load_weight,
+    date_left: req.body.date_left,
+    tavtsan_ashiglalt: req.body.tavtsan_ashiglalt,
+    puulelt: req.body.puulelt,
+    forklift_usage: req.body.forklift_usage,
+    crane_usage: req.body.crane_usage,
+    fine1: req.body.fine1,
+    fine2: req.body.fine2,
+    other1: req.body.other1,
+    other2: req.body.other2,
+    invoice_to_302: req.body.invoice_to_302,
+    invoice_to_601: req.body.invoice_to_601,
+    amount_w_noat: req.body.amount_w_noat,
+    amount_wo_noat: req.body.amount_wo_noat,
+    client_name: req.body.client_name,
   });
 
   res.status(200).json({ message: "Order created", order });
@@ -25,11 +41,10 @@ const updateOrder = asyncHandler(async (req, res) => {
     throw new Error("Order not found!");
   }
 
-  console.log(req.body);
-
   const updatedOrder = await Order.findByIdAndUpdate(
     req.params.id,
     {
+    ...req.body,
       truck_num: {
         digits: req.body.truck_num_digits,
         letters: req.body.truck_num_letters,
--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -12,6 +12,54 @@ const truckSchema = mongoose.Schema(
         required: [true, "Арлын дугаарын үсгийг оруулна уу!"],
       }, 
     },
+    load_name: {
+      type: String,
+    },
+    load_weight: {
+      type: Number
+    },
+    date_left: {
+      type: Date
+    },
+    tavtsan_ashiglalt: {
+      type: String
+    },
+    puulelt: {
+      type: Boolean
+    },
+    hour_forklift_used: {
+      type: String
+    },
+    crane_usage: {
+      type: Number
+    },
+    fine1: {
+      type: Boolean
+    },
+    fine2: {
+      type: Boolean
+    },
+    other1: {
+      type: Boolean
+    },
+    other2: {
+      type: Boolean
+    },
+    invoice_to_302: {
+      type: Number
+    },
+    invoice_to_601: {
+      type: Number
+    },
+    amount_w_noat: {
+      type: Number
+    },
+    amount_wo_noat: {
+      type: Number
+    },
+    client_name: {
+      type: String
+    }
   },
   {
     timestamps: true,
