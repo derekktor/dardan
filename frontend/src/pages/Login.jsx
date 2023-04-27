15b81e22d6511d31c55852c0ed33ac25bf0a120b
Derek <baldka1109bh@gmail.com>
2023-04-27 21:50:35 +0800

    Copy and modify Register for Login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,69 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { useState } from "react";
+import { FaSignInAlt } from "react-icons/fa";
 
-export default Login
+const Login = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
+  });
+
+  const { name, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    alert("Submitted!")
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Nevtreh
+        </h1>
+        <p>Medeellee oruulna uu!</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Ner"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Nuuts ug"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Login;
