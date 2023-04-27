873c56dfee3a2973fc1c292631a125889624c952
Derek <baldka1109bh@gmail.com>
2023-04-27 21:45:17 +0800

    Basic layout for Register, submitting

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,76 @@
-const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
 
-export default Register
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, password, password2 } = formData;
+
+  const onChange = () => {};
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    alert("Submitted!")
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Burtguuleh
+        </h1>
+        <p>Burtguulne uu!</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Ner"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Nuuts ug"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password2"
+              name="password2"
+              value={password2}
+              placeholder="Nuuts ug davtah"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Register;
