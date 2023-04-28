f9c6baddbd5811e05bfeb089e7c85f216c5775b7
Derek <baldka1109bh@gmail.com>
2023-04-28 16:04:59 +0800

    Navigate to login page if not logged in

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,21 @@
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-export default Dashboard
+const Dashboard = () => {
+  const navigate = useNavigate();
+  const {user} = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    // if not logged in, navigate to login page
+    if (!user) {
+      navigate("/login");
+    }
+
+    
+  }, [user, navigate])
+
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
